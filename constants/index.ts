31bd0c844f2745b3940ec18b8d705f1dbd753ded
Felipe Simões <ffelipesimoes@gmail.com>
2022-06-11 20:57:56 +0100

    build: deploy contract on rinkeby

---
+++ constants/index.ts
@@ -0,0 +1,7 @@
+// Address of the Whitelist Contract that you deployed
+export const WHITELIST_CONTRACT_ADDRESS = "0x0D1f970Ab5709B733F5141911A44a6d40bAdB864";
+// URL to extract Metadata for a Crypto Dev NFT
+export const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+// module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
+
