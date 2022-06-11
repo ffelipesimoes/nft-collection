228329bed49d27686256dd7d619261241b401ad3
Felipe Simões <ffelipesimoes@gmail.com>
2022-06-11 19:49:06 +0100

    feat: set typescript as default

---
+++ hardhat.config.ts
@@ -0,0 +1,31 @@
+import '@typechain/hardhat'
+import '@nomiclabs/hardhat-ethers'
+import '@nomiclabs/hardhat-waffle'
+
+import { HardhatUserConfig } from "hardhat/config";
+
+const config: HardhatUserConfig = {
+  // Your type-safe config goes here
+};
+
+export default config;
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+};
--- my-app/.next/static/chunks/webpack.js
+++ my-app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1ef9bf500c524060"; }
+/******/ 		__webpack_require__.h = function() { return "02794f4c50858fc1"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
